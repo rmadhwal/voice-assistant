eb5022cb66f78bd792965f0b2038018a7c22f6e2
rmadhwal <rohan@madhwal.com>
2023-04-20 23:23:02 +0200

    use threading so we're always listening

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import ffmpeg
 import numpy
 from vosk import Model, KaldiRecognizer, SetLogLevel
 from gtts import gTTS
+import _thread
 
 SAMPLE_RATE = 16000
 SILENCE_THRESHOLD = 100000
@@ -26,8 +27,10 @@ numpy.set_printoptions(threshold=sys.maxsize, linewidth=sys.maxsize)
 voice_commands_enabled = False
 waiting_for_song = False
 song_is_playing = False
+command_failed = False
 greetings = ["hello", "hi", "hey", "hola", "whatsup", "sup"]
-song_commands = {"play_command": "play", "pause_command": "pause", "cancel_command": "cancel", "next_command": "next", "rewind_command": "rewind", "back_command": "back", "search_command": "search"}
+song_commands = {"play_command": "play", "pause_command": "pause", "cancel_command": "cancel", "next_command": "next",
+                 "rewind_command": "rewind", "back_command": "back", "search_command": "search"}
 speaker_commands = {"off_command": "off"}
 ai_name = "buddy"
 disable_command = ["disable", "goodbye"]
@@ -37,6 +40,7 @@ local_ip_address = s.getsockname()[0]
 host = '192.168.0.100'
 port = 50001
 previous_outputs = []
+accepted_output = ""
 empty_outputs = 0
 last_output_time = time.time()
 last_accepted_output = ""
@@ -47,6 +51,7 @@ rtp_address = 'rtp://' + local_ip_address + ':5555'
 model = Model(lang="en-us")
 rec = KaldiRecognizer(model, SAMPLE_RATE)
 process1 = ffmpeg.input(rtp_address).output('-', format='s16le', ac=1, ar='16k').run_async(pipe_stdout=True)
+
 
 def speak(text):
     textobj = gTTS(text=text, lang=language, slow=False, tld='co.jp')
@@ -87,6 +92,7 @@ def turn_bt_mode_on_speakers():
 
 
 def parse_command(final_output):
+    global command_failed
     words = final_output.split(" ")
     if bool(set(disable_command) & set(words)):
         speak("Disabling Voice Commands, Goodbye")
@@ -120,6 +126,7 @@ def parse_command(final_output):
             speak("enabling bluetooth")
             turn_bt_mode_on_speakers()
     else:
+        command_failed = True
         speak("I did not recognize that command, please try again")
 
 
@@ -135,55 +142,93 @@ def search_and_play_song(final_output):
     waiting_for_song = False
 
 
-while process1.poll() is None:
-    packet = process1.stdout.read(packet_size)
-    try:
-        if rec.AcceptWaveform(packet):
-            final_outputs += [json.loads(rec.Result())['text']]
-        else:
-            partial_outputs += [json.loads(rec.PartialResult())['partial']]
-        final_outputs = list(filter(None, final_outputs))
-        partial_outputs = list(filter(None, partial_outputs))
-        final_output = ""
-        if len(final_outputs) > 0:
-            max_len = 0
-            for output in final_outputs:
-                if len(output) > max_len:
-                    final_output = output
-                    max_len = len(output)
-        elif len(partial_outputs) > 0:
-            max_len = 0
-            for output in partial_outputs:
-                if len(output) > max_len:
-                    final_output = output
-                    max_len = len(output)
-            if final_output == last_partial_output:
-                final_output = ""
+def see_if_command_exists():
+    global accepted_output
+    global process1
+
+    while process1.poll() is None:
+        if accepted_output != "":
+            print(accepted_output)
+            if not voice_commands_enabled:
+                enable_voice_commands(accepted_output)
+            elif not waiting_for_song:
+                parse_command(accepted_output)
             else:
-                last_partial_output = final_output
-        if final_output != "":
-            previous_outputs += [final_output]
-        elif len(previous_outputs) > 1:
-            empty_outputs += 1
-            new_previous_outputs = []
-            for i in range(len(previous_outputs)):
-                words = previous_outputs[i].split(" ")
-                if not(bool(set(last_accepted_output) & set(words))) or time.time() - last_output_time > 20:
-                    new_previous_outputs.append(previous_outputs[i])
-            previous_outputs = new_previous_outputs
-            if empty_outputs > 20 and len(previous_outputs) > 1:
-                if not voice_commands_enabled:
-                    enable_voice_commands(previous_outputs[-1])
-                elif not waiting_for_song:
-                    parse_command(previous_outputs[-1])
+                search_and_play_song(accepted_output)
+            accepted_output = ""
+
+
+def listen_for_commands():
+    global final_outputs
+    global partial_outputs
+    global last_partial_output
+    global previous_outputs
+    global empty_outputs
+    global last_accepted_output
+    global last_output_time
+    global voice_commands_enabled
+    global waiting_for_song
+    global accepted_output
+    global process1
+    global command_failed
+
+    while process1.poll() is None:
+        packet = process1.stdout.read(packet_size)
+        try:
+            if rec.AcceptWaveform(packet):
+                final_outputs += [json.loads(rec.Result())['text']]
+            else:
+                partial_outputs += [json.loads(rec.PartialResult())['partial']]
+            final_outputs = list(filter(None, final_outputs))
+            partial_outputs = list(filter(None, partial_outputs))
+            final_output = ""
+            if len(final_outputs) > 0:
+                max_len = 0
+                for output in final_outputs:
+                    if len(output) > max_len:
+                        final_output = output
+                        max_len = len(output)
+            elif len(partial_outputs) > 0:
+                max_len = 0
+                for output in partial_outputs:
+                    if len(output) > max_len:
+                        final_output = output
+                        max_len = len(output)
+                if final_output == last_partial_output:
+                    final_output = ""
                 else:
-                    search_and_play_song(previous_outputs[-1])
-                last_accepted_output = list(filter(lambda x : x != 'song' and x != 'speakers', previous_outputs[-1].split(" ")))
-                last_output_time = time.time()
-                previous_outputs = []
-        final_outputs = []
-        partial_outputs = []
-    except socket.error:
-        process1.stdout.close()
-        process1.wait()
-        break
+                    last_partial_output = final_output
+            if final_output != "":
+                previous_outputs += [final_output]
+            elif len(previous_outputs) > 1:
+                empty_outputs += 1
+                new_previous_outputs = []
+                for i in range(len(previous_outputs)):
+                    words = previous_outputs[i].split(" ")
+                    if not (bool(set(last_accepted_output) & set(words))) or time.time() - last_output_time > 20:
+                        new_previous_outputs.append(previous_outputs[i])
+                previous_outputs = new_previous_outputs
+                if empty_outputs > 20 and len(previous_outputs) > 1:
+                    accepted_output = previous_outputs[-1]
+                    if not command_failed:
+                        last_accepted_output = list(
+                            filter(lambda x: x != 'song' and x != 'speakers', previous_outputs[-1].split(" ")))
+                        last_output_time = time.time()
+                        command_failed = False
+                    previous_outputs = []
+            final_outputs = []
+            partial_outputs = []
+        except socket.error:
+            process1.stdout.close()
+            process1.wait()
+            break
+
+
+try:
+    _thread.start_new_thread(listen_for_commands, ())
+    _thread.start_new_thread(see_if_command_exists, ())
+except:
+    print("Error: unable to start thread")
+
+while 1:
+   pass
